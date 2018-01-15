2a89c7e73ce632007420bbd690d1275e6c2e7f0b
Shivani Patel <shivanidp7@gmail.com>
2018-01-15 11:34:16 -0800

    [bugfix] --heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path'); 
 
 module.exports = {
-    entry: path.resolve(__dirname, './client/src/index.js'),
+    entry: path.resolve(__dirname, '/client/src/index.js'),
     output: {
-        path: path.resolve(__dirname, './client/public/'),
+        path: path.resolve(__dirname, '/client/public/'),
         filename: 'bundle.js'
     }, 
     module: {
