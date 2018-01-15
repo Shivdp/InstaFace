58882411d88aa8f194ea2b22192a31a41a2a0dfd
Shivani Patel <shivanidp7@gmail.com>
2018-01-15 10:45:02 -0800

    [bugfix] --heroku

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['env', 'react']
+                    presets: ['env', 'es2015', 'react']
                 }
             }
         ]
