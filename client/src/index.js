46b7ef38f80f8fa2c7c16d9257b2e4f82a66bac0
Shivani Patel <shivanidp7@gmail.com>
2018-01-14 23:30:13 -0800

    [bugfix] --working on heroku still

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 
 import React from 'react'; 
 import { render } from 'react-dom'; 
-import App from './Components/app';
+import App from './Components/App';
 import allReducers from './reducers';
 
 import { createStore } from 'redux';
